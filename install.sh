09e887af9ae8cce55e6a2a811a8f396f124eda4f
Ambrus Attila <attila.ambrus0022@gmail.com>
2022-08-20 17:37:27 +0200

    install.sh is working now

--- install.sh
+++ install.sh
@@ -1,7 +1,64 @@
 #!/bin/bash
+DOTFILES=$(pwd)
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
 
-rm -f ~/.vimrc
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
 
-rm -f ~/.zshrc
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+error() {
+    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+    exit 1
+}
+
+warning() {
+    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+info() {
+    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+}
+
+success() {
+    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+get_linkables() {
+    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+}
+
+setup_symlinks() {
+	title "Creating symlinks"
+
+for file in $(get_linkables); do
+	target="$HOME/.$(basename "$file" '.symlink')"
+        if [ -e "$target" ]; then
+            info "~${target#$HOME} already exists... Skipping."
+        else
+            info "Creating symlink for $file"
+            ln -s "$file" "$target"
+        fi
+done
+
+}
+
+
+case "$1" in
+		link)
+			setup_symlinks
+			;;
+		*)
+			echo -e $"\nUsage: $(basename "$0") {link}\n"
+			exit 1
+			;;
+esac
+
+echo -e
+success "Done."
