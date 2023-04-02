8adb18d6018fd831ffe9f758d54d413ac585e728
Ambrus Attila <70056837+v1szk1s@users.noreply.github.com>
2023-04-02 15:38:49 +0200

    keymap, ctrl-jkl harpoon duplicate keybind with window movement, removed window movement keybind

--- nvim.config/lua/hulu/init.lua
+++ nvim.config/lua/hulu/init.lua
@@ -1,8 +1,2 @@
 require("hulu.set")
 require("hulu.remap")
-
-vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        dofile(os.getenv("DOTFILES") .. "/nvim.config/lua/hulu/remap.lua")
-    end,
-})
--- nvim.config/lua/hulu/remap.lua
+++ nvim.config/lua/hulu/remap.lua
@@ -53,9 +53,3 @@ vim.keymap.set("n", "<leader>'", "viw<esc>a'<esc>bi'<esc>lel")
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
-
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
