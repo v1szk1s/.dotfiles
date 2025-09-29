c83db22fe2f8bacb6073991dbf8b646b822fc4a5
Attila Ambrus <attila.ambrus0022@gmail.com>
2025-09-29 21:26:25 +0200

    adding force option to install.sh

--- install.sh
+++ install.sh
@@ -1,75 +1,58 @@
 #!/bin/bash
 DOTFILES=$(pwd)
+FORCE=false
+
+# Parse args
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--force)
+      FORCE=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
 
 setup_gitconfig(){
-	if [[ ! -f $HOME/.gitconfig ]]; then
-		echo "Making symlink for gitconfig"
-		ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+	if [[ ! -f $HOME/.gitconfig || $FORCE == true ]]; then
+		echo "Linking gitconfig"
+		ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
 	else
-		echo "Already exists... Skipping. "
+		echo "Already exists... Skipping."
 	fi
 }
 
 setup_tmux(){
-
-	if [[ ! -e $DOTFILES/tmux.config/plugins/tpm ]]; then
+	if [[ ! -e $DOTFILES/tmux.config/plugins/tpm || $FORCE == true ]]; then
+		if [[ -d $DOTFILES/tmux.config/plugins/tpm ]]; then
+			rm -rf $DOTFILES/tmux.config/plugins/tpm
+		fi
 		git clone https://github.com/tmux-plugins/tpm $DOTFILES/tmux.config/plugins/tpm
-		success "Done"
+		echo "tmux plugins installed."
 	else
-		echo "Already exists... Skipping. "
+		echo "Already exists... Skipping."
 	fi
-
 }
 
 setup_symlinks() {
-	for i in $(find $DOTFILES -maxdepth 1 -name '*.config'); do
-		basename=$(basename "$DOTFILES/$i" '.config')
+	for i in $(find "$DOTFILES" -maxdepth 1 -name '*.config'); do
+		basename=$(basename "$i" '.config')
+		target="$HOME/.config/$basename"
 
-		target="$HOME/.config/$(basename "$DOTFILES/$i" '.config')"
-
-		if [[ ! -e $target ]]; then
+		if [[ ! -e $target || $FORCE == true ]]; then
 			echo "Creating $basename symlink."
-			ln -sf $i $target
-
-			[[ "$i" =~ zsh ]] && echo "ln -sf $i/zshenv $HOME/.zshenv"
+			ln -sfn "$i" "$target"
+			[[ "$basename" == "zsh" ]] && ln -sfn "$i/zshenv" "$HOME/.zshenv"
 		else
-			echo "Already exists... Skipping. "
+			echo "Already exists... Skipping."
 		fi
 	done
 }
 
+setup_gitconfig
+setup_tmux
 setup_symlinks
 
-
-# POSITIONAL_ARGS=()
-#
-# while [[ $# -gt 0 ]]; do
-#   case $1 in
-#     -e|--extension)
-#       EXTENSION="$2"
-#       shift # past argument
-#       shift # past value
-#       ;;
-#     -s|--searchpath)
-	#       SEARCHPATH="$2"
-	#       shift # past argument
-	#       shift # past value
-	#       ;;
-#     --default)
-	#       DEFAULT=YES
-	#       shift # past argument
-	#       ;;
-#     -*|--*)
-	#       echo "Unknown option $1"
-	#       exit 1
-	#       ;;
-#     *)
-	#       POSITIONAL_ARGS+=("$1") # save positional arg
-	#       shift # past argument
-	#       ;;
-	#   esac
-	# done
-	#
-	# set -- "${POSITIONAL_ARGS[@]}"
-
-	echo -e "Done"
+echo -e "Done"
