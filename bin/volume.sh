716049f06b018b89ed51b2a1edfff52a0c4980c5
Attila Ambrus <attila.ambrus0022@gmail.com>
2025-10-10 09:31:03 +0200

    adding limit to volume

--- bin/volume.sh
+++ bin/volume.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-wpctl set-volume @DEFAULT_AUDIO_SINK@ $1
 
-VOL_PERCENTAGE=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}')
+wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ $1
+
+
+VOL_PERCENTAGE=$(
+  wpctl get-volume @DEFAULT_AUDIO_SINK@ \
+  | awk '{printf("%d\n", $2 * 100 + 0.5)}'
+)
 
 notify-send \
     -h string:x-canonical-private-synchronous:sys \
